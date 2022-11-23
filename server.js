7070311783ef86c3a366b4467750a7d8b87551c0
fliderman01 <fliderman01@gmail.com>
2022-11-23 15:15:50 +0400

    restored first version

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3002;
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+// middleware
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -16,13 +19,35 @@ app.use((req, res, next) => {
 app.use("/api/workouts", workoutRoutes);
 
 app.get("/", (req, res) => {
-  res.send("EXPERIMENT! port1: " + { PORT });
+  const routesText = `
+  <h1>Available methods</h1>
+  <div>
+    <pre><b>Check ./routes.js</b></pre>
+  </div>
+  
+  <pre>
+  WORKOUT:
+
+  router.post('/api/workout'),
+  router.get('/api/ workout/:id'),
+  router.get('/api/workouts’),
+  router.patch('/api/workouts/:id),
+  router.delete('/api/workouts/:id),
+  </pre>
+    `;
+  res.status(200).send(routesText);
 });
 
-mongoose.connect(process.env.MONGO_URI).catch((err) => {
-  console.log(err);
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(
+        `Connecter to DB and listening on port http://localhost:${PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
